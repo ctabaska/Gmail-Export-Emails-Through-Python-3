8aeff2dd6b6d7284fa13ac49787f97869e4ce15c
Chris Tabaska <christophertabaska@gmail.com>
2020-05-03 00:03:57 -0600

    Batch Requests and Frequency tsv
    Restructured the program to use batch requests in chunks of 40 messages
    Added the ability to create a frequency tsv file for easier understanding

--- quickstart.py
+++ quickstart.py
@@ -5,7 +5,6 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from apiclient import errors
-import csv
 from time import time
 from time import sleep
 
@@ -14,11 +13,66 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
 """Get a list of Messages from the user's mailbox.
 """
+def splitString(string): # Takes a string as input and splits it in between the less than and greater sign
+    if(len(string.split('<')) > 1):
+        return string.split('<')[1].split('>')[0]
+    return string
+    
+
+def callback(response_id, response, exception):
+    print(response)
+
+    sender = 'null'
+    subject = 'null'
+    recipient = 'null'
+    
+    if ('labelIds' in response): # Checks if there are 'labelIds'
+        if('CHAT' in response.get('labelIds')): # 'Chat' emails get rejected because they don't contain enough information to be useful
+            return None
+    else:
+        return None
+
+    if ('payload' in response and 'headers' in response.get('payload')):
+        for header in response.get('payload').get('headers'):
+            if (header['name'] == 'To'):
+                recipient = splitString(header['value'])
+            elif (header['name'] == 'From'):
+                sender = splitString(header['value'])
+            elif (header['name'] == 'Subject'):
+                subject = header['value']
+    else:
+        print('Error finding \'payload\' in response')
+    
+    if (sender == 'null'):
+        print ('\'From\' header could not be found')
+
+    if (recipient == 'null'):
+        print ('\'To\' header could not be found')
+    
+    if (subject == 'null'):
+        print ('\'Subject\' header could not be found')
+
+
+    print(sender + ' -> ' + recipient + ' - ' + subject)
+    with open('emails.tsv', 'a', newline='', encoding='utf-8') as tsvfile:
+        tsvfile.write(response.get('id') + '\t' + sender + '\t' + recipient + '\t' + subject + '\n')
+    print('\n')
+
+
+
+def createBatch(service, callback, id_list): # Creates a batch http request with all the ids recieved and then returns it
+    batch = service.new_batch_http_request(callback)
+    for id_pair in id_list:
+        temp_id = id_pair.get('id')
+        batch.add(service.users().messages().get(userId='me', id=temp_id, format='metadata', metadataHeaders=['From', 'Subject', 'To']))
+    return batch
+
+def getEmailList(service, numResults, nextPageToken=None): # Gets a list of all the messages in the next stack
+    return service.users().messages().list(userId='me', maxResults=numResults, pageToken=nextPageToken).execute()
+
 
 def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
+
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -39,57 +93,48 @@ def main():
 
     service = build('gmail', 'v1', credentials=creds)
 
-    startTime = time()
-    endTime = 'null'
-   
-    """
-    Testing for one loop that breaks when there is 'none' return from listing
-    """
-    pageToken = 'null'
-    resultsPerThread = 500
-    count = 0
-    with open('emails.tsv', 'w+', newline='', encoding='utf-8') as csvfile:
-        csvfile.write('Sender\tSubject\tTimestamp\tSnippet\n')
-        while True:
-            msgList = 'null'
-            idList = 'null'
-            if (pageToken == None):
-                break
-            elif (pageToken == 'null'):
-                print ("This thread's token: " + str(pageToken))
-                msgList = service.users().messages().list(userId='me', labelIds=['UNREAD'], maxResults=resultsPerThread).execute()
-            else:
-                print ("This thread's token: " + str(pageToken))
-                msgList = service.users().messages().list(userId='me', labelIds=['UNREAD'], maxResults=resultsPerThread, pageToken=pageToken).execute()
-            idList = msgList.get('messages') # Breaks the message list down into just the list of 'id' and the actual id
-            for idDict in idList:
-                count += 1
-                print ("Parsing message #" + str(count) + " with messageId: " + idDict['id'])
-                tempMsg = service.users().messages().get(userId='me', id=idDict['id'], format='metadata').execute()
-                sender = 'null'
-                subject = 'null'
-                timestamp = 'null'
-                snippet = 'null' 
-                for rename in tempMsg['payload']['headers']:
-                    if (rename['name'] == 'From'):
-                        sender = rename['value']
-                    elif (rename['name'] == 'Subject'):
-                        subject = rename['value']
-                timestamp = tempMsg['internalDate']
-                snippet = tempMsg['snippet']
-                if ('\t' in sender or '\t' in subject or '\t' in snippet):
-                    print("RED ALERT")
-                csvfile.write(sender + '\t' + subject +  '\t' + timestamp + '\t' + snippet + '\n')
-            pageToken = msgList.get('nextPageToken')
-    endTime = time()
-    timeElapsed = endTime - startTime
-    hours = int(timeElapsed / (60 * 60))
-    minutes = int((timeElapsed - (hours * 60 * 60) )/ 60)
-    seconds = round(timeElapsed % (60), ndigits=2)
-    print ("Total Count: " + str(count) + " messages")
-    print ("Time Elapsed: " + str(hours) + "h " + str(minutes) + "m " + str(seconds) + "s")
-    
+    nextPageToken = None # Next page token is used to find the next set of messages in the stack of emails
+    batchCount = 0
 
+    # Opens 'emails.tsv' and creates a header
+    with open('emails.tsv', 'w+', newline='', encoding='utf-8') as tsvfile:
+        tsvfile.write('Message Id\tSender\tRecipient\tSubject\n')
+    
+    while True:
+        batchCount += 1
+        msgsList = getEmailList(service, 40, nextPageToken)
+
+        nextPageToken = msgsList.get('nextPageToken')
+
+        id_list = msgsList.get('messages')
+
+        batch = createBatch(service, callback, id_list)
+        
+        batch.execute()
+        print('Batch number: ' + str(batchCount))
+        if(nextPageToken == None):
+            break
+    
+    emailCount = {}
+    count = 0
+
+    # Loops through 'emails.tsv' to check frequency of who you got emails from
+    with open('emails.tsv', 'r') as emailList:
+        while True:
+            test = emailList.readline()
+            if (test == ''): # Check if eof
+                break
+            test = test.split('\t')[1]
+
+            if (test not in emailCount): 
+                count += 1
+                emailCount[test] = 1
+            else:
+                emailCount[test] += 1
+
+    with open('email_count.tsv', 'w+', newline='', encoding='utf-8') as tsvfile:
+        for keyValue in emailCount:
+            tsvfile.write(keyValue + '\t' + str(emailCount.get(keyValue)) + '\n')
 
 
     
